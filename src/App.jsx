da38dd3b4e34cd795de59541a3023491e98b628d
kaushal kumar <kaushalkumar50009@gmail.com>
2023-09-23 03:14:32 +0530

    generating password

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // State variables to manage password generation and options
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+  // Ref to store a reference to the password input field
+  const passRef = useRef(null)
+
+  // Function to generate a random password based on user options
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMONPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*(){}[]?<>_-~'+/"
+
+    for (let i = 1; i <= length; i++) {
+      const char = Math.floor(Math.random() * str.length) // Fixed index out of range issue
+      pass += str.charAt(char)
+      setPassword(pass)
+    }
+  }, [length, numberAllowed, charAllowed, setPassword])
+
+  // Function to copy the generated password to the clipboard
+  const copyPassToClipBoard = useCallback(() => {
+    passRef.current?.select()
+    passRef.current?.setSelectionRange(0, 999) // Select the entire text
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  // useEffect to trigger password generation when options change
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-8 py-4 my-8 text-orange-500 bg-gray-700">
+        <h1 className='text-center text-white text-5xl my-6'>
+          Password Generator
+        </h1>
+
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className='outline-none w-full h-14 py-1 px-3'
+            readOnly
+            placeholder='password'
+            ref={passRef}
+          />
+
+          <button
+            className='outline-none bg-blue-600 text-white px-3 py-0.5 shrink'
+            onClick={copyPassToClipBoard}
+          >
+            Copy
+          </button>
+        </div>
+
+        <div className="flex text-sm gap-x-2 justify-around">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              value={length}
+              min={6}
+              max={100}
+              className='cursor-pointer'
+              onChange={(e) => { setLength(e.target.value) }}
+            />
+            <label htmlFor="length" className='text-white text-lg'>Length : {length}</label>
+          </div>
+
+          <div className="flex items-center gap-x-3">
+            <input
+              type="checkbox"
+              value={numberAllowed}
+              id='numberInput'
+              defaultChecked={numberAllowed}
+              onChange={() => { setNumberAllowed(prev => !prev) }}
+            />
+            <label htmlFor="numberInput" className='text-white text-lg'>Number</label>
+          </div>
+
+          <div className="flex items-center gap-x-3">
+            <input
+              type="checkbox"
+              value={charAllowed}
+              id='charInput'
+              defaultChecked={charAllowed}
+              onChange={() => { setCharAllowed(prev => !prev) }}
+            />
+            <label htmlFor="charInput" className='text-white text-lg'>Special_Char</label>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
